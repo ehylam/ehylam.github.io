2b0057e500528e578a028bf64c62fa266baaef17
Eric Lam <ehylam.p@gmail.com>
2019-10-29 19:33:07 +1100

    initial commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,4 @@
+// @codekit-prepend "src/TimelineMax.min.js","src/aos.js", "src/TweenMax.min.js","src/ScrollMagic.min.js","src/animation.gsap.js","src/slick.min.js";
+$(document).ready(function() {
+	AOS.init();
+});
