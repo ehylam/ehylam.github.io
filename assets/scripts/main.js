6ed88e6f5a60818383c1dda002597938375f7d33
Eric Lam <ehylam.p@gmail.com>
2019-10-29 20:27:46 +1100

    Doge commit

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	AOS.init();
 	function main() {
 		const canvas = document.querySelector('#c');
-		const renderer = new THREE.WebGLRenderer({ canvas });
+		const renderer = new THREE.WebGLRenderer({ alpha: true });
 
 		const fov = 75;
 		const aspect = 2; // the canvas default
