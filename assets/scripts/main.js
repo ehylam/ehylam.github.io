cd41edd358f86fa3687760dd1b33daacaaf592cb
Eric Lam <ehylam.p@gmail.com>
2019-11-03 11:18:45 +1100

    Removed js play function

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,9 +1,7 @@
 // @codekit-prepend "src/jquery-3.4.1.min.js" , "src/three.min.js","src/TimelineMax.min.js","src/aos.js", "src/TweenMax.min.js","src/ScrollMagic.min.js","src/animation.gsap.js";
 $(document).ready(function() {
 	AOS.init();
-	var myVideo = document.getElementsByTagName('video');
 
-	myVideo.play();
 	$(function() {
 		$('.meme-container')
 			.mousemove(function(e) {
